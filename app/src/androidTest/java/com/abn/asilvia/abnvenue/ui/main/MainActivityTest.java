415875f81c06691dea83939b25e0780b7ba3e798
asilvia <asilvia3@gmail.com>
2018-05-22 09:20:33 +0200

    added unit simple unit testing

---
+++ app/src/androidTest/java/com/abn/asilvia/abnvenue/ui/main/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.abn.asilvia.abnvenue.ui.main;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by asilvia on 21/05/2018.
+ */
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    private MainActivity mainActivity= null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testSearchLounch()
+    {
+        View search = mainActivity.mActivityStartBinding.tvLastSearched;
+        assertNotNull(search);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+
+}
